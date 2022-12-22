a177270fa91a93f81ad7b416c2bab975b0c76fb9
champ <kkongsil@outlook.com>
2022-12-22 22:07:20 +0700

    u1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { JsonPlaceholderTestService } from './json-placeholder-test.service';
+import { from, of, map } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -14,5 +15,37 @@ export class AppComponent {
     this.jsonPlaceholderTestService.getPost().subscribe((results) => {
       console.log(results);
     });
+  }
+
+  newPost() {
+    let request_data = [
+      {
+        title: 'r1',
+        body: 'r2',
+        userId: 1,
+      },
+      {
+        title: 'r2',
+        body: 'r2',
+        userId: 2,
+      },
+      {
+        title: 'r3',
+        body: 'r3',
+        userId: 3,
+      },
+    ];
+
+    //https://blog.angular-university.io/rxjs-higher-order-mapping/
+
+    from(request_data)
+      .pipe(map((x) => this.jsonPlaceholderTestService.newPost(x)))
+      .subscribe((v) => console.log(v));
+    //
+    // this.jsonPlaceholderTestService
+    //   .newPost(request_data)
+    //   .subscribe((results) => {
+    //     console.log(results);
+    //   });
   }
 }
--- src/app/json-placeholder-test.service.ts
+++ src/app/json-placeholder-test.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -7,7 +8,16 @@ import { HttpClient } from '@angular/common/http';
 export class JsonPlaceholderTestService {
   constructor(private http: HttpClient) {}
 
+  private resource_uri = 'https://jsonplaceholder.typicode.com/posts';
   getPost() {
-    return this.http.get('https://jsonplaceholder.typicode.com/posts');
+    return this.http.get(this.resource_uri);
+  }
+
+  newPost(request_data: any) {
+    const httpOptions = {
+      headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+    };
+
+    return this.http.post(this.resource_uri, request_data, httpOptions);
   }
 }
