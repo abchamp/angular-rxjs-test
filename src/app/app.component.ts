b5b4579b3abc7bdd072995dc30bbc9f270045cb7
champ <kkongsil@outlook.com>
2022-12-24 16:13:23 +0700

    up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,58 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  AfterContentInit,
+  AfterViewInit,
+  ViewChild,
+  ElementRef,
+} from '@angular/core';
 import { JsonPlaceholderTestService } from './json-placeholder-test.service';
-import { from, of, map } from 'rxjs';
+import {
+  from,
+  of,
+  map,
+  concatMap,
+  concat,
+  mergeMap,
+  fromEvent,
+  startWith,
+  debounceTime,
+  distinctUntilChanged,
+} from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'my-app';
+  @ViewChild('filterInput') filterElementRef!: ElementRef;
 
   constructor(private jsonPlaceholderTestService: JsonPlaceholderTestService) {}
+
+  ngAfterViewInit(): void {
+    fromEvent(this.filterElementRef.nativeElement, 'keyup')
+      .pipe(
+        map((event: any) => event.target.value),
+        startWith(''),
+        debounceTime(400),
+        distinctUntilChanged()
+      )
+      .subscribe(console.log);
+  }
 
   getPost() {
     this.jsonPlaceholderTestService.getPost().subscribe((results) => {
       console.log(results);
     });
+  }
+
+  testConcatPost() {
+    const series1$ = of('a', 'b');
+    const series2$ = of('x', 'y');
+
+    const result$ = concat(series1$, series2$);
+    result$.subscribe(console.log);
   }
 
   newPost() {
@@ -38,10 +76,17 @@ export class AppComponent {
 
     //https://blog.angular-university.io/rxjs-higher-order-mapping/
 
-    from(request_data)
-      .pipe(map((x) => this.jsonPlaceholderTestService.newPost(x)))
-      .subscribe((v) => console.log(v));
+    // from(request_data)
+    //   .pipe(map((x) => this.jsonPlaceholderTestService.newPost(x)))
+    //   .subscribe((v) => console.log(v));
     //
+    // from(request_data)
+    //   .pipe(concatMap((req) => this.jsonPlaceholderTestService.newPost(req)))
+    //   .subscribe((saveResult) => console.log(saveResult));
+    //
+    from(request_data)
+      .pipe(mergeMap((req) => this.jsonPlaceholderTestService.newPost(req)))
+      .subscribe((saveResult) => console.log(saveResult));
     // this.jsonPlaceholderTestService
     //   .newPost(request_data)
     //   .subscribe((results) => {
